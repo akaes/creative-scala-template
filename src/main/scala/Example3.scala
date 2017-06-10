ca6d926c0b86c53a28ee6b337688d38926bcafe3
Andrea Knabe-S <a-k-s@web.de>
2017-06-10 14:27:28 +0200

    Example3
    val Declarations

---
+++ src/main/scala/Example3.scala
@@ -0,0 +1,13 @@
+import doodle.core._
+import doodle.core.Image._
+import doodle.syntax._
+import doodle.jvm.Java2DFrame._
+import doodle.backend.StandardInterpreter._
+
+object Example3 {
+
+	val one = 1
+	val anImage = Image.circle(100).fillColor(Color.red)
+	
+}
+
